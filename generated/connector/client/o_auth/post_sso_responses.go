49cfa7f3a77dc09fd408a71f114256148fd3c90d
Tim Speed <tim@manifold.co>
2018-07-11 13:54:11 -0300

    Copied the primitive from marketplace so I didn't need to reference

--- generated/connector/client/o_auth/post_sso_responses.go
+++ generated/connector/client/o_auth/post_sso_responses.go
@@ -11,7 +11,7 @@ import (
 	"github.com/go-openapi/errors"
 	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/validate"
-	"github.com/manifoldco/marketplace/connector/primitives"
+	"github.com/manifoldco/grafton/marketplace/primitives"
 
 	strfmt "github.com/go-openapi/strfmt"
 )
