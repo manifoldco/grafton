ad0834294816cfdde5eb268746419023e1a09dee
Luiz Branco <me@luizbranco.com>
2019-04-11 11:26:40 -0700

    Update lint tools

--- tools.go
+++ tools.go
@@ -6,7 +6,7 @@ import (
 	"github.com/alecthomas/gometalinter"
 	"github.com/client9/misspell/cmd/misspell" // lint
 	"github.com/go-swagger/go-swagger/cmd/swagger"
-	"github.com/golang/lint/golint"      // lint
+	"golang.org/x/lint/golint"           // lint
 	"github.com/gordonklaus/ineffassign" // lint
 	"github.com/tsenart/deadcode"        // lint
 )
